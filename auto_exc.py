afb0fe95ce7b6e1ea2f41ea3c23af5e205bcc86f
GenyaElamkov <genyaElamkov@gmail.com>
2025-02-15 15:25:13 +0300

    insert field organization

--- auto_exc.py
+++ auto_exc.py
@@ -1,5 +1,7 @@
 import os
 import gc
+import threading
+
 
 from tqdm import tqdm
 from openpyxl import Workbook, load_workbook
@@ -14,6 +16,10 @@ def find_xlsx_files(directory: str) -> list:
     ]
 
 
+def create_directory(name: str):
+    os.makedirs(name, exist_ok=True)
+
+
 class Book:
     def __init__(self) -> None:
         self.counter_row = 1  # Счетчик для row
@@ -22,22 +28,22 @@ class Book:
         """Обрезает цифры, оставляет буквы для Ячеек"""
         return "".join([char for char in text if not char.isdigit()])
 
-    def set_book(self, filename: str) -> dict[str]:
+    def reed_book(self, filename: str) -> dict[str]:
         wb = load_workbook(filename=filename, read_only=True)
         # read_only — режим для отложенной загрузки, экономить оперативную память
         sheet_ranges = wb.active
+        
+        book = {}
+        organization = sheet_ranges['B25'].value
+        
         order = []
         for coll in sheet_ranges["C32":"V32"]:
             for cell in coll:
-                if not cell.value:
-                    continue
                 order.append(cell.value)
         order = "".join(order)
 
-        id_cell = 43
-        counter_col = 0  # Счетчик для col
-        book = {}
-
+        id_cell = 43        # Счетчик для row обработки таблицы
+        counter_col = 0     # Счетчик для col
         while True:
             id_cell += 1
             id_coll_start = f"A{id_cell}"
@@ -50,39 +56,47 @@ class Book:
                         book.setdefault(f"{coordinate}{self.counter_row}", cell.value)
                         counter_col += 1
                     book.setdefault(f"AW{self.counter_row}", order)
-
+                    book.setdefault(f"AX{self.counter_row}", organization)
                     counter_col = 0
                 self.counter_row += 1
             else:
+                self.counter_row = 1
                 gc.collect()  # Чистим мусор
                 wb.close()  # Закрываем книгу
                 break
         return book
 
-    def create_new_book(self, data: dict[str]) -> None:
+    def create_new_book(self, data: dict[str], file_name: str) -> None:
         wb_new = Workbook()
 
         sheet = wb_new.active
         sheet.title = "Сводный отчет"
         for k, v in data.items():
             sheet[k] = v
-        wb_new.save("report.xlsx")
+        wb_new.save(file_name)
+        wb_new.close()
 
 
-def main():
-    data = {}
-
+def main(path, name_directory):
     bk = Book()
-    file_list = find_xlsx_files(os.getcwd())
-    # file_list = find_xlsx_files(r"C:\Projects\auto_exc\data_1")
-    for path in tqdm(file_list):
-        data.update(bk.set_book(path))
-    
-    bk.create_new_book(data)
+    data = bk.reed_book(path)
+    path_directory = f"{name_directory}\\{path.split("\\")[-1]}"
+    bk.create_new_book(data, file_name=path_directory)
 
 
 if __name__ == "__main__":
     try:
-        main()
+        name_directory = "00_Data"
+        create_directory(name_directory)
+        file_list = find_xlsx_files(os.getcwd())
+        theards = []
+        for path in tqdm(file_list):
+            th = threading.Thread(target=main, args=(path, name_directory), daemon=True)
+            th.start()
+            theards.append(th)
+
+        for th in tqdm(theards):
+            th.join()
+
     except Exception as e:
         print(f"Ошибка {e}")
